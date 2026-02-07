4797d70ed041e8044d90fe686558fab78c89ddd6
TheSergi15 <sergiovalengamer54@gmail.com>
2026-02-07 19:49:44 +0100

    cambios script menu

--- RPMI_Project2EVA_25-26/Assets/Scripts/MainMenu.cs
+++ RPMI_Project2EVA_25-26/Assets/Scripts/MainMenu.cs
@@ -5,21 +5,27 @@ public class MainMenu : MonoBehaviour
 {
     [Header("Panels")]
     public GameObject optionsPanel;
+    
+
     public void PlayGame()
     {
         SceneManager.LoadScene("SERGI_TEST");
     }
+
     public void OpenOptions()
     {
         optionsPanel.SetActive(true);
     }
+
     public void CloseOptions()
     {
         optionsPanel.SetActive(false);
     }
+
     public void QuitGame()
     {
         Application.Quit();
-        Debug.Log("Se cierra el juego");
+        Debug.Log("El juego se cerraría aquí");
     }
 }
+
